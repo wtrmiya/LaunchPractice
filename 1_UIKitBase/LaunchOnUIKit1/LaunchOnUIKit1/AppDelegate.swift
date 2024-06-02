9ef90100021e017cab527eb477cc37535091ad74
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-02 11:18:14 +0900

    chore: #1 SwiftLintを設定した close #1

--- 1_UIKitBase/LaunchOnUIKit1/LaunchOnUIKit1/AppDelegate.swift
+++ 1_UIKitBase/LaunchOnUIKit1/LaunchOnUIKit1/AppDelegate.swift
@@ -9,11 +9,12 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = ViewController()
         self.window = window
@@ -22,4 +23,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
