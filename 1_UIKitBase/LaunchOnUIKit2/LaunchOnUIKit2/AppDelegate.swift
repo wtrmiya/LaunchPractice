5deb509dc137f6068cc0753e20a820919b021913
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-03 10:52:56 +0900

    chore: #8 AppDelegateとSceneDelegateにデリゲートメソッドを記載し、ログ出力可能にした close #8

--- 1_UIKitBase/LaunchOnUIKit2/LaunchOnUIKit2/AppDelegate.swift
+++ 1_UIKitBase/LaunchOnUIKit2/LaunchOnUIKit2/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import OSLog
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,5 +27,74 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    // MARK: - Responding to app life-cycle events
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    // MARK: - Continuing user activity and handling quick actions
+    func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping (
+            [any UIUserActivityRestoring]?
+        ) -> Void
+    ) -> Bool {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+        return true
+    }
+    
+    func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFailToContinueUserActivityWithType userActivityType: String,
+        error: any Error
+    ) {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    @MainActor
+    func application(
+        _ application: UIApplication,
+        performActionFor shortcutItem: UIApplicationShortcutItem
+    ) async -> Bool {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+        return true
+    }
+    
+    // MARK: - Opening a URL-specified resource
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        Logger.appDelegate.info("file: \(#file), function: \(#function)")
+        return true
     }
 }
