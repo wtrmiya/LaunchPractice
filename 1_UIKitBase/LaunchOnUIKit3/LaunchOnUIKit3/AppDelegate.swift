61de6b93ced8dbfee162822b95c2fa6742bf114b
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-03 15:55:33 +0900

    chore: #11 デリゲートメソッドとロガーを設定した close #11

--- 1_UIKitBase/LaunchOnUIKit3/LaunchOnUIKit3/AppDelegate.swift
+++ 1_UIKitBase/LaunchOnUIKit3/LaunchOnUIKit3/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import OSLog
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,5 +27,74 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    // MARK: - Responding to app life-cycle events
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    // MARK: - Continuing user activity and handling quick actions
+    func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+        Logger.appDelegate.info(#function)
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping (
+            [any UIUserActivityRestoring]?
+        ) -> Void
+    ) -> Bool {
+        Logger.appDelegate.info(#function)
+        return true
+    }
+    
+    func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFailToContinueUserActivityWithType userActivityType: String,
+        error: any Error
+    ) {
+        Logger.appDelegate.info(#function)
+    }
+    
+    @MainActor
+    func application(
+        _ application: UIApplication,
+        performActionFor shortcutItem: UIApplicationShortcutItem
+    ) async -> Bool {
+        Logger.appDelegate.info(#function)
+        return true
+    }
+    
+    // MARK: - Opening a URL-specified resource
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        Logger.appDelegate.info(#function)
+        return true
     }
 }
