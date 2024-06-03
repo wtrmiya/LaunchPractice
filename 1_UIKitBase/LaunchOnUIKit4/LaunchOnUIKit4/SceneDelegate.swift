58f01ce2d3afa6c63c328c010d8bfba6dd2ca28c
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 08:37:38 +0900

    chore: #43 LaunchOnUIKit4 SceneDelegateにログを設定した

--- 1_UIKitBase/LaunchOnUIKit4/LaunchOnUIKit4/SceneDelegate.swift
+++ 1_UIKitBase/LaunchOnUIKit4/LaunchOnUIKit4/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OSLog
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -15,6 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = UIHostingController(rootView: MainView())
@@ -23,17 +26,42 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
+    // MARK: - Transition to the foreground
     func sceneWillEnterForeground(_ scene: UIScene) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    // MARK: - Transition to the background
+    func sceneWillResignActive(_ scene: UIScene) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
     }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
+    // MARK: - Opening URLs
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    // MARK: - Continuing user activities
+    func scene(_ scene: UIScene, willContinueUserActivityWithType userActivityType: String) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
+    }
+    
+    func scene(_ scene: UIScene, didFailToContinueUserActivityWithType userActivityType: String, error: any Error) {
+        Logger.sceneDelegate.info("file: \(#file), function: \(#function)")
     }
 }
