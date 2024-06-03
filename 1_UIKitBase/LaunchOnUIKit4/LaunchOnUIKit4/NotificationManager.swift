bbd7d7d29b97b24823d032ac9e73c2e2be0301e8
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 04:18:46 +0900

    chore: #15 LaunchOnUIKit4 Local Pushを設定した

---
+++ 1_UIKitBase/LaunchOnUIKit4/LaunchOnUIKit4/NotificationManager.swift
@@ -0,0 +1,51 @@
+//
+//  NotificationManager.swift
+//  LaunchOnUIKit4
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import UserNotifications
+import OSLog
+
+final class NotificationManager {
+    static let shared = NotificationManager()
+    private let logger = Logger(subsystem: Logger.subsystem, category: String(describing: NotificationManager.self))
+    
+    func requestAuthorization() {
+        logger.info("file: \(#file), function: \(#function)")
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { [weak self] _, error in
+            if let error {
+                self?.logger.info("ERROR: \(error.localizedDescription)")
+            } else {
+                self?.logger.info("SUCCESS")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        logger.info("file: \(#file), function: \(#function)")
+        let content = UNMutableNotificationContent()
+        content.title = "Launch Practice"
+        content.subtitle = "Local Notification"
+        content.sound = .default
+        content.badge = 0
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(request)
+    }
+    
+    func cancelNotification() {
+        logger.info("file: \(#file), function: \(#function)")
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+    }
+}
