f0d34e9692cac37ecc93f376aaec4314cbad6eb6
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 04:26:07 +0900

    chore: #15 LaunchOnUIKit4 Widgetを設定した close #15

---
+++ 1_UIKitBase/LaunchOnUIKit4/LaunchOnUIKit4Widget/LaunchOnUIKit4WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchOnUIKit4WidgetBundle.swift
+//  LaunchOnUIKit4Widget
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LaunchOnUIKit4WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LaunchOnUIKit4Widget()
+    }
+}
