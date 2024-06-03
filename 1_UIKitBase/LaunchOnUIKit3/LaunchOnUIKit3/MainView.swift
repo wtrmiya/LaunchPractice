1be2ff45fc1f30da35930446792e58ad7c5d7c11
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-03 15:37:15 +0900

    chore: #10 Storybaordを削除した

---
+++ 1_UIKitBase/LaunchOnUIKit3/LaunchOnUIKit3/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  LaunchOnUIKit3
+//
+//  Created by Wataru Miyakoshi on 2024/06/03.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("MainView")
+    }
+}
+
+#Preview {
+    MainView()
+}
