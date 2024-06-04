0e2d5ffcc0ce8a5ea1587791debceddd92cd628c
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 10:31:42 +0900

    chore: #39 LaunchOnUIKit5 MainViewにScenePhaseとログを設定した close #38

--- 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/MainView.swift
+++ 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/MainView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import OSLog
 
 struct MainView: View {
+    private let logger = Logger(subsystem: Logger.subsystem, category: String(describing: MainView.self))
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some View {
         Text("MainView")
+            .padding()
+            .onChange(of: scenePhase, perform: { phase in
+                switch phase {
+                case .active:
+                    logger.info("file: \(#file), active")
+                case .inactive:
+                    logger.info("file: \(#file), inactive")
+                case .background:
+                    logger.info("file: \(#file), background")
+                @unknown default:
+                    logger.info("file: \(#file), @unknown")
+                }
+            })
     }
 }
 
