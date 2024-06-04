50e5acccfe15302fdbc099940717b6a993633bea
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 14:07:28 +0900

    chore: #21 LaunchOnSwiftUI2 Widgetを設定した close #21

---
+++ 2_SwiftUIBase/LaunchOnSwiftUI2/LaunchOnSwiftUI2Widget/LaunchOnSwiftUI2WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchOnSwiftUI2WidgetBundle.swift
+//  LaunchOnSwiftUI2Widget
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LaunchOnSwiftUI2WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LaunchOnSwiftUI2Widget()
+    }
+}
