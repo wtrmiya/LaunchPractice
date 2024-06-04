42a134d3502733ce7622cf759a924c6b0f5138ed
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 13:39:45 +0900

    chore: #19 LaunchOnSwiftUI2 Xcodeのプロジェクトを作成した

---
+++ 2_SwiftUIBase/LaunchOnSwiftUI2/LaunchOnSwiftUI2/LaunchOnSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchOnSwiftUI2App.swift
+//  LaunchOnSwiftUI2
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import SwiftUI
+
+@main
+struct LaunchOnSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
