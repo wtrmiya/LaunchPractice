69515f4011ee5fcc0f77f6716dafc7a6a14868ba
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 09:10:17 +0900

    chore: #37 LaunchOnUIKit5 Storyboardを削除した

---
+++ 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  LaunchOnUIKit5
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("MainView")
+    }
+}
+
+#Preview {
+    MainView()
+}
