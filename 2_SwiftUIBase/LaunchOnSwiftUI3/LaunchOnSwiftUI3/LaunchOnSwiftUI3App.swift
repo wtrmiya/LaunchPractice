bd1fdec1197b0afc87a073e79a9c3ca062af4c2e
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 14:12:28 +0900

    chore: #22 LaunchOnSwiftUI3 Xcodeプロジェクトを作成した

---
+++ 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/LaunchOnSwiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchOnSwiftUI3App.swift
+//  LaunchOnSwiftUI3
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import SwiftUI
+
+@main
+struct LaunchOnSwiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
