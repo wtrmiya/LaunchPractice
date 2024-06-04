c7c7d25533d4a01489f5dbe32632d4cb3dc1d532
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 10:23:17 +0900

    chore: #37 LaunchOnUIKit5 SceneDelegateを不使用にした close #37

--- 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/AppDelegate.swift
+++ 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/AppDelegate.swift
@@ -5,26 +5,21 @@
 //  Created by Wataru Miyakoshi on 2024/06/04.
 //
 
-import UIKit
+import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: MainView())
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
