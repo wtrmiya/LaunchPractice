0e77161c163da5189e6d733934e9622b06c696d6
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 14:26:53 +0900

    chore: #22 LaunchOnSwiftUI3 SceneDelegateを設定した

---
+++ 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  LaunchOnSwiftUI3
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import SwiftUI
+
+final class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+      
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+      
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+      
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+      
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+      
+    }
+}
