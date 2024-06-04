815fb81462305580f712b58d2d5c74f51afaaedb
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 13:45:02 +0900

    chore: #19 LaunchOnSwiftUI2 SwifLintを設定した close #19

--- 2_SwiftUIBase/LaunchOnSwiftUI2/LaunchOnSwiftUI2/AppDelegate.swift
+++ 2_SwiftUIBase/LaunchOnSwiftUI2/LaunchOnSwiftUI2/AppDelegate.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
         return true
     }
 }
