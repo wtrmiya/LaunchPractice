8eb7716be1d6aa2500f7a225c076dada34e325f7
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 10:43:13 +0900

    chore: #39 LaunchOnUIKit5 Local Pushを設定した

--- 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/MainView.swift
+++ 1_UIKitBase/LaunchOnUIKit5/LaunchOnUIKit5/MainView.swift
@@ -14,20 +14,42 @@ struct MainView: View {
     @Environment(\.scenePhase) private var scenePhase
     
     var body: some View {
-        Text("MainView")
-            .padding()
-            .onChange(of: scenePhase, perform: { phase in
-                switch phase {
-                case .active:
-                    logger.info("file: \(#file), active")
-                case .inactive:
-                    logger.info("file: \(#file), inactive")
-                case .background:
-                    logger.info("file: \(#file), background")
-                @unknown default:
-                    logger.info("file: \(#file), @unknown")
-                }
+        VStack {
+            Spacer()
+            Button(action: {
+                NotificationManager.shared.requestAuthorization()
+            }, label: {
+                Text("Request Auth")
             })
+            Spacer()
+                .frame(height: 20)
+            Button(action: {
+                NotificationManager.shared.scheduleNotification()
+            }, label: {
+                Text("Schedule Local Push")
+            })
+            Spacer()
+                .frame(height: 20)
+            Button(action: {
+                NotificationManager.shared.cancelNotification()
+            }, label: {
+                Text("Cancel Schedule")
+            })
+            Spacer()
+        }
+        .padding()
+        .onChange(of: scenePhase, perform: { phase in
+            switch phase {
+            case .active:
+                logger.info("file: \(#file), active")
+            case .inactive:
+                logger.info("file: \(#file), inactive")
+            case .background:
+                logger.info("file: \(#file), background")
+            @unknown default:
+                logger.info("file: \(#file), @unknown")
+            }
+        })
     }
 }
 
