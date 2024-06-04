5d125bdef9be2e12fa3d272a1cd59ffe452539cf
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 14:16:00 +0900

    chore: #22 LaunchOnSwiftUI3 AppDelegateを設定した

---
+++ 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  LaunchOnSwiftUI3
+//
+//  Created by Wataru Miyakoshi on 2024/06/04.
+//
+
+import SwiftUI
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
