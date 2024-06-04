180b78c055e63e9ccc149032c3273df81375ae13
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-04 14:36:52 +0900

    chore: #22 LaunchOnSwiftUI3 SwiftLintを設定した close #22

--- 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/AppDelegate.swift
+++ 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/AppDelegate.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
         return true
     }
     
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let sceneConfig: UISceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfig: UISceneConfiguration = UISceneConfiguration(
+            name: nil,
+            sessionRole: connectingSceneSession.role
+        )
         sceneConfig.delegateClass = SceneDelegate.self
         return sceneConfig
     }
--- 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/SceneDelegate.swift
+++ 2_SwiftUIBase/LaunchOnSwiftUI3/LaunchOnSwiftUI3/SceneDelegate.swift
@@ -8,30 +8,28 @@
 import SwiftUI
 
 final class SceneDelegate: NSObject, UIWindowSceneDelegate {
-    
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard (scene as? UIWindowScene) != nil else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-      
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-      
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-      
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-      
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-      
     }
 }
